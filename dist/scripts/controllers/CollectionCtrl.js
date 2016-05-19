862c3be237ac3a322394209b28f79a32a2b8868d
Marc Bakker <marc@burotijs.nl>
2016-05-19 15:34:11 +0200

    assignment 6 changes

--- dist/scripts/controllers/CollectionCtrl.js
+++ dist/scripts/controllers/CollectionCtrl.js
@@ -1,17 +1,7 @@
 (function() {
-//     function CollectionCtrl() {
-//        this.albums = [];
-//        for (var i=0; i < 12; i++) {
-//            this.albums.push(angular.copy(albumPicasso));
-//        }
-//     }
     
     function CollectionCtrl(Fixtures) {
-        this.albums = [];
-        for (var i=0; i < 12; i++) {
-            var data = Fixtures.getAlbum();
-            this.albums.push(angular.copy(Fixtures.getAlbum()));
-        }
+        this.albums = Fixtures.getCollection(15);
      }
  
      angular
