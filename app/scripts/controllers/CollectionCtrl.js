1b4726779f38ebb1f1556a6b27ce63708f90abba
Marc Bakker <marc@burotijs.nl>
2016-05-19 14:45:41 +0200

    checkpoint 6 changes

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,12 +1,20 @@
 (function() {
-     function CollectionCtrl() {
+//     function CollectionCtrl() {
+//        this.albums = [];
+//        for (var i=0; i < 12; i++) {
+//            this.albums.push(angular.copy(albumPicasso));
+//        }
+//     }
+    
+    function CollectionCtrl(Fixtures) {
         this.albums = [];
         for (var i=0; i < 12; i++) {
-            this.albums.push(angular.copy(albumPicasso));
+            var data = Fixtures.getAlbum();
+            this.albums.push(angular.copy(Fixtures.getAlbum()));
         }
      }
  
      angular
          .module('blocJams')
-         .controller('CollectionCtrl', CollectionCtrl);
+         .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
  })();
