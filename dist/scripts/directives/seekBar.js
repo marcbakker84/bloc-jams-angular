44b297aa911dbee287c6784e38e6d83fc1613225
Marc Bakker <marc@burotijs.nl>
2016-06-02 14:15:19 +0200

    checkpoint 10 changes

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -14,12 +14,22 @@
             templateUrl: '/templates/directives/seek_bar.html',
             replace: true,
             restrict: 'E',
-            scope: { },
+            scope: {
+                onChange: '&'
+            },            
             link: function(scope, element, attributes) {
                 scope.value = 0;
                 scope.max = 100;
  
                 var seekBar = $(element);
+                
+                attributes.$observe('value', function(newValue) {
+                    scope.value = newValue;
+                });
+ 
+                attributes.$observe('max', function(newValue) {
+                    scope.max = newValue;
+                });
                 
                 var percentString = function () {
                     var value = scope.value;
@@ -27,9 +37,7 @@
                     var percent = value / max * 100;
                     return percent + "%";
                 };
-                
-                console.log(percentString)
- 
+                 
                 scope.fillStyle = function() {
                     return {width: percentString()};    
                 };
@@ -41,16 +49,24 @@
                 scope.onClickSeekBar = function(event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
+                    notifyOnChange(scope.value);
                 };
                 
                 scope.trackThumb = function() {
                     $document.bind('mousemove.thumb', function(event) {
                         var percent = calculatePercent(seekBar, event);
-                       scope.$apply(function() {
+                    scope.$apply(function() {
                         scope.value = percent * scope.max;
+                        notifyOnChange(scope.value);
                     });
                         //scope.value = percent * scope.max;
                 });
+                    
+                var notifyOnChange = function(newValue) {
+                    if (typeof scope.onChange === 'function') { //JUKE --> when do you add this kind of checks? 
+                        scope.onChange({value: newValue});
+                    }
+                };
                      
                  $document.bind('mouseup.thumb', function() {
                     $document.unbind('mousemove.thumb');
--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {  
+    function SongPlayer($rootScope, Fixtures) {
         /**
         * @desc ??
         * @type ??
@@ -26,6 +26,7 @@
          var setSong = function(song) {
             if (currentBuzzObject) {
                 stopSong();
+                SongPlayer.currentSong.playing = null;
             }
  
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -33,6 +34,13 @@
                 preload: true
             });
  
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+            });
+             
+             
             SongPlayer.currentSong = song;
         };
         
@@ -72,6 +80,12 @@
         * @type {Object}
         */    
         SongPlayer.currentSong = null;
+        
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
         
         /**
         * @method SongPlayer.play
@@ -134,12 +148,22 @@
                 }
             };  
     
-         
+        /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
+        
         return SongPlayer;
         // JUKE --> why do we return songplayer and what is SongPlayer?    
      }
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
